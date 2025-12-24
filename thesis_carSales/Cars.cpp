66dff91fa9b8ab325c271944a5ec6b9e64915ef7
dudolad_yaroslav <dudolad2009@gmail.com>
2025-12-24 21:45:48 +0200

    Realized "Cars" cpp file

--- thesis_carSales/Cars.cpp
+++ thesis_carSales/Cars.cpp
@@ -1,1 +1,38 @@
 #include "Cars.h"
+#include <iostream>
+
+Cars::Cars()
+{
+	brand = "Undefined";
+	year = 0;
+	price = 0.0;
+	equipment = "None";
+	country = "Undefined";
+	saleDate = { 0, 0, 0 };
+	fullName = "Undefined";
+}
+
+Cars::Cars(std::string brand, int year, float price, std::string equipment, std::string country, Date saleDate, std::string fullName)
+{
+	this->brand = brand;
+	this->year = year;
+	this->price = price;
+	this->equipment = equipment;
+	this->country = country;
+	this->saleDate = saleDate;
+	this->fullName = fullName;
+}
+
+Cars::~Cars() {}
+
+void Cars::Print()
+{
+	std::cout << "Brand: " << brand << std::endl;;
+	std::cout << "Year: " << year << std::endl;;
+	std::cout << "Price: " << price << std::endl;;
+	std::cout << "Equipment: " << equipment << std::endl;;
+	std::cout << "Country: " << country << std::endl;;
+	std::cout << "Sale date: " << saleDate.saleDay << saleDate.saleMonth
+		<< saleDate.saleYear << std::endl;
+	std::cout << "Buyer: " << fullName << std::endl;
+}
