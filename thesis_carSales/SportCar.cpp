759ffc8a03ea6914b1e6c42b0f4dbcac23575c85
dudolad_yaroslav <dudolad2009@gmail.com>
2026-01-08 17:00:08 +0200

    Realized class "SportCar"

---
+++ thesis_carSales/SportCar.cpp
@@ -0,0 +1,38 @@
+#include "SportCar.h"
+#include <stdexcept>
+#include <iostream>
+
+SportCar::SportCar(std::string brand, int year, float price, std::string equipment, std::string country, Date saleDate, std::string buyerFullName, float sekTo100km, float displacement, int enginePower)
+    :Cars(brand, year, price, equipment, country, saleDate, buyerFullName),
+    sekTo100km(sekTo100km), displacement(displacement), enginePower(enginePower)
+{
+    if (sekTo100km <= 0)
+        throw std::invalid_argument("Time to 100km/h must be positive");
+    if (displacement <= 0)
+        throw std::invalid_argument("Engine displacement must be positive");
+    if (enginePower <= 0)
+        throw std::invalid_argument("Engine power must be positive");
+}
+
+float SportCar::getSekTo100km() const
+{
+    return sekTo100km;
+}
+
+float SportCar::getDisplacement() const
+{
+    return displacement;
+}
+
+int SportCar::getEnginePower() const
+{
+    return enginePower;
+}
+
+void SportCar::Print() const
+{
+    Cars::Print(); 
+    std::cout << "Time to 100 km/h: " << sekTo100km << " sec" << std::endl;
+    std::cout << "Engine displacement: " << displacement << " L" << std::endl;
+    std::cout << "Engine power: " << enginePower << " HP" << std::endl;
+}
---
+++ thesis_carSales/SportCar.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "Cars.h"
+#include <string>
+class SportCar :
+    public Cars
+{
+protected:
+    float sekTo100km;
+    float displacement;
+    int enginePower;
+public:
+    SportCar(std::string brand, int year, float price, std::string equipment,
+        std::string country, Date saleDate, std::string buyerFullName,
+        float sekTo100km, float displacement, int enginePower);
+
+    float getSekTo100km() const;
+    float getDisplacement() const;
+    int getEnginePower() const;
+
+    void Print() const override;
+
+};
+
